5654f11c11db6de45417b0d8fd0708e1f794cf76
Kaushik Jadav <kaushikjadav602@gmail.com>
2018-04-27 15:06:38 +0530

    Update settings.py

--- corto/settings.py
+++ corto/settings.py
@@ -78,10 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'corto.wsgi.application'
 
-
-Database
-https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
